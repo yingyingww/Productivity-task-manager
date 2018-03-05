eb6bc00681dac8fad3775904f23209c2e232d2c1
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-03-05 17:54:50 -0600

    Update TimerLogic.java

--- src/Main/TimerLogic.java
+++ src/Main/TimerLogic.java
@@ -1,42 +1,42 @@
-package Main;// Timer Logic (Model)
+package Main;
+
+// controller
+// Timer Logic (Model)
 // Start/stop timer function// Ask for productivity when stop is called
 // Store task, start, stop, and productivity
 // Find total time
+// still need to work on finding the time interval
 
-
-
-
-//import java.util.TimerTask;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-//import java.util.Timer;
+
 
 public class TimerLogic {
-    public Date time;
-    public long startTime;
-    public long endTime;
-    public long timeInterval;
+    private Date startTime;
+    private Date endTime;
 
-
-    //public Timer myTimer = new Timer();
-    //private int minutes;
-
-    //public TimerTask task = new TimerTask(){
-    //public void run(){
-    //minutes++;
-
-    //}
-//System.currentTimeMillis()
     public void start(){
-        startTime = System.currentTimeMillis();
-        System.out.println("Start time is " + startTime);
+        Calendar startCal = Calendar.getInstance();
+        startTime = startCal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println("Start Time is: " + sdf.format(startTime) ) ;
     }
 
     public void end(){
-        endTime = System.currentTimeMillis();
-        timeInterval = endTime -startTime;
-        System.out.println("End time is "+endTime);
-        System.out.println("Time interval is  "+ timeInterval);
+        Calendar endCal = Calendar.getInstance();
+        endTime = endCal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println("End Time is: " + sdf.format(endTime));
+        //timeInterval(startTime, endTime);
 
     }
+
+    //private void timeInterval(Date startTime, Date endTime) {
+        //long duration = end - start;
+        //System.out.println("start:"+start + "end:"+end+"Time Interval is: " + duration);
+
+     // }
+
 
 }
