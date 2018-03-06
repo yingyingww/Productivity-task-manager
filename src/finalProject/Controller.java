ac839d8c34020a7062b33cbf7d47d944d3bf5cc2
Eva Grench <egrench@eduroam-163-47.dyn.carleton.edu>
2018-03-05 21:38:20 -0600

    Adding controller

---
+++ src/finalProject/Controller.java
@@ -0,0 +1,20 @@
+package finalProject;
+
+import java.sql.Time;
+import java.util.ArrayList;
+
+public class Controller {
+
+    public ArrayList calendarRectangle(ArrayList taskAttributes) {
+        ArrayList rectangleAttributes = new ArrayList();
+
+        String taskName = (String) rectangleAttributes.get(0);
+        int startHour = (int) rectangleAttributes.get(1);
+        if(rectangleAttributes.get(3) == "PM") {
+            startHour += 12;
+        }
+        Time startTime = new Time(startHour, (int) rectangleAttributes.get(2), 0);
+
+        return rectangleAttributes;
+    }
+}
