5a1e218d42babd31419997f2c5a9cd746eed7deb
Andrew <biehla@gmail.com>
2018-03-09 14:25:25 -0600

    changed IdealScheduleModel

--- src/finalProject/IdealScheduleModel.java
+++ src/finalProject/IdealScheduleModel.java
@@ -1,6 +1,13 @@
 package finalProject;
 
-public class IdealScheduleModel {
+import java.util.ArrayList;
+import java.util.List;
 
+public class IdealScheduleModel {
+    private List<ArrayList> taskInstances = new ArrayList<ArrayList>();
+
+    public void addTaskInstance(ArrayList taskAttributes) {
+        taskInstances.add(taskAttributes);
+    }
 
 }
