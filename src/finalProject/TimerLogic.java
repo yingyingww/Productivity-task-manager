824a4652736ad1fe03580b49081e30ec0a16373e
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-03-09 13:24:26 -0600

    Update TimerLogic.java

--- src/finalProject/TimerLogic.java
+++ src/finalProject/TimerLogic.java
@@ -1,42 +1,59 @@
 package finalProject;
 
 // controller
-// Timer Logic (Model)
-// Start/stop timer function// Ask for productivity when stop is called
+// Ask for productivity when stop is called
 // Store task, start, stop, and productivity
-// Find total time
-// still need to work on finding the time interval
+
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
 public class TimerLogic {
     private Date startTime;
     private Date endTime;
+    //private Calendar startCal;
 
-    public void start(){
+    public Date logStartTime(){
         Calendar startCal = Calendar.getInstance();
         startTime = startCal.getTime();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        System.out.println("Start Time is: " + sdf.format(startTime) ) ;
+        System.out.println("Start Time is: " + sdf.format(startTime) );
+        return startTime;
     }
 
-    public void end(){
+    public Date logEndTime(){
         Calendar endCal = Calendar.getInstance();
         endTime = endCal.getTime();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         System.out.println("End Time is: " + sdf.format(endTime));
-        //timeInterval(startTime, endTime);
-
+        return endTime;
     }
 
-    //private void timeInterval(Date startTime, Date endTime) {
-        //long duration = end - start;
-        //System.out.println("start:"+start + "end:"+end+"Time Interval is: " + duration);
+    public long getTimeInterval(){
+        long intervalInMs = endTime.getTime() - startTime.getTime();
+        long intervalInMinutes = TimeUnit.MILLISECONDS.toMinutes(intervalInMs);
+        long intervalInSeconds = TimeUnit.MILLISECONDS.toSeconds(intervalInMs);
+        System.out.println("Time Interval is: " + intervalInSeconds + " seconds");
+        System.out.println("Time Interval is: " + intervalInMinutes + " minutes");
+        return intervalInMinutes;
+    }
 
-     // }
-
-
+     public static void main(String[] args) {
+        TimerLogic a = new TimerLogic();
+        a.logStartTime();
+        //wait for 3 secs
+         try {
+             Thread.sleep(3000);
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+         a.logEndTime();
+         a.getTimeInterval();
+     }
 }
+
+
+
