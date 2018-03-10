412fc306cd6b40a1035996b0f4dc687181c151b4
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-03-09 19:24:32 -0600

    Update Task.java

--- src/finalProject/Task.java
+++ src/finalProject/Task.java
@@ -3,9 +3,12 @@ package finalProject;
 public class Task {
     private String name;
     private boolean isRunning = false;
+    private Main mainPage;
+    private TimerLogic aTimer = new TimerLogic();
 
-    public Task(String name) {
+    public Task(String name, Main mainPage) {
         this.name = name;
+        this.mainPage = mainPage;
     }
 
     public String getName() {
@@ -26,13 +29,13 @@ public class Task {
 
     // The following two methods will be used to call other classes.
     private void startTask() {
-        //filler code to start...
+        aTimer.logStartTime();
         String beginTaskStatement = "Task " + this.getName() + " has begun.";
         System.out.println(beginTaskStatement);
     }
 
     private void endTask() {
-        //filler code to start...
+        aTimer.logEndTime();
         String endTaskStatement = "Task " + this.getName() + " has ended.";
         System.out.println(endTaskStatement);
     }
