eb1dfe0b4a00dd3752b5a1b7c87cdafe7224a300
Andrew <biehla@gmail.com>
2018-03-12 15:22:16 -0500

    clean up

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -1,8 +1,6 @@
 package finalProject;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
