16f6900c6168aa7fac5f4236c15c99cd5a553d8f
calypsoLeonard <35149366+calypsoLeonard@users.noreply.github.com>
2018-03-14 00:37:23 -0500

    Update TimeComparator.java

--- src/finalProject/TimeComparator.java
+++ src/finalProject/TimeComparator.java
@@ -1,12 +1,14 @@
 package finalProject;
 
 import java.util.Comparator;
-
+/*
+A class to allow Task objects to be compared and sorted according to total time spent.
+*/
 public class TimeComparator implements Comparator<Task>{
     @Override
-    public int compare(Task o1, Task o2) {
-        int length1 = o1.getTotalTimeSpent();
-        int length2 = o2.getTotalTimeSpent();
+    public int compare(Task task1, Task task2) {
+        int length1 = task1.getTotalTimeSpent();
+        int length2 = task2.getTotalTimeSpent();
         return length1 > length2 ? 1:
                 length2 > length1 ? -1:
                         0;
