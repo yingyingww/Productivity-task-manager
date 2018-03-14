51a0c482ae41ea47a19de78fca7f023a41b13304
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-03-14 09:23:45 -0500

    Update Task.java

--- src/finalProject/Task.java
+++ src/finalProject/Task.java
@@ -79,45 +79,4 @@ public class Task {
         return (avgProductivity / taskOccurrences.size());
     }
 
-//TODO delete stuff below
-//    //maybe the worst piece of code I have ever written
-//    private ArrayList<Object> createTaskAttributes(String name, Date start, Date end) {
-//        ArrayList<Object> taskAttributes = new ArrayList<>();
-//
-//        Label nameInput = new Label(name);
-//
-//        Calendar startcal = GregorianCalendar.getInstance();
-//        startcal.setTime(start);
-//        String amPm;
-//        int startHour = startcal.get(Calendar.HOUR_OF_DAY);
-//        if (startHour > 12) {
-//            amPm = "AM";
-//        } else {
-//            amPm = "PM";
-//            startHour = startHour - 12;
-//        }
-//
-//        Object startHoursInput = (Object) startHour;
-//        Object startMinutesInput = (Object) Integer.toString(startcal.get(Calendar.MINUTE));
-//        Object startAMPMInput = (Object) amPm;
-//
-//        Calendar endcal = GregorianCalendar.getInstance();
-//        startcal.setTime(start);
-//        String amPm2;
-//        int endHour = startcal.get(Calendar.HOUR_OF_DAY);
-//        if (endHour > 12) {
-//            amPm2 = "AM";
-//        } else {
-//            amPm2 = "PM";
-//        }
-//        endHour = startHour%12;
-//
-//        Object endHoursInput = (Object) endHour;
-//        Object endMinutesInput = (Object) Integer.toString(endcal.get(Calendar.MINUTE));
-//        Object endAMPMInput = (Object) amPm2;
-//
-//        taskAttributes.addAll(Arrays.asList(nameInput, startHoursInput, startMinutesInput, startAMPMInput, endHoursInput, endMinutesInput, endAMPMInput));
-//        return taskAttributes;
-//    }
-
 }
